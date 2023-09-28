5774c342a34c30dbbfe98ac3eccca972cc2e314d
Vibol <nubvibol@gmail.com>
2023-09-28 16:51:33 +0700

    API works (Implement api to interface)

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,50 +1,41 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
    // Loading({super.key});
-
   @override
   State<Loading> createState() => _LoadingState();
 }
-
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/Asia/Bangkok'));
-    Map data = jsonDecode(response.body);
-    //print(data);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+  String time = 'Loading';
 
-    print(datetime);
-    print(offset);
-
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getTime();
+    setupWorldTime();
     //print('Hey There');
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Padding(
+        padding: EdgeInsets.all(40),
         child: Column(
           children: [
-            TextButton.icon(onPressed: (){
-              getTime();
-            }, icon: Icon(Icons.play_circle), label: Text('Run'))
+            Text('$time')
           ],
         ),
       ),
